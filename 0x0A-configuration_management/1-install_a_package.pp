03dbb42bf41be01dac3ffc34f76dbb580e2400ff
Jesica-akeyo <jessica.akeyo@gmail.com>
2022-09-16 19:08:46 +0000

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # this Puppet manifest installs the puppet-lint package
-package {'puppet-lint':
-ensure   => '2.1.1',
-provider => 'gem',
+package {'Flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
